9e7cc295e92fb9a1bcc967b38aa04b061157bb99
Carolin <raketensofa@hotmail.com>
2016-06-14 21:50:57 +0200

    14.06.2016 21:50

--- app/src/main/java/com/cgellner/yomm/DialogFragments/DF_NewTrans.java
+++ app/src/main/java/com/cgellner/yomm/DialogFragments/DF_NewTrans.java
@@ -3,18 +3,23 @@ package com.cgellner.yomm.DialogFragments;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.cgellner.yomm.Activities.MainActivity;
 import com.cgellner.yomm.GlobalVar;
 import com.cgellner.yomm.Objects.MainCategory;
 import com.cgellner.yomm.Objects.Person;
@@ -27,6 +32,7 @@ import com.synnapps.carouselview.ViewListener;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 import javax.microedition.khronos.opengles.GL;
 
@@ -34,13 +40,12 @@ import javax.microedition.khronos.opengles.GL;
 /**
  * Created by Carolin on 31.05.2016.
  */
-public class DF_NewTrans extends DialogFragment {
+
+public class DF_NewTrans extends Fragment {
 
     private final String TAG = DF_NewTrans.class.getName();
 
-
-    CarouselView carouselView;
-    int[] carouselViews = {R.layout.layout_new_first_whichvalue,
+    int[] views = {R.layout.layout_new_first_whichvalue,
                     R.layout.layout_new_second_persons,
                     R.layout.layout_new_third_personpayed,
                     R.layout.layout_new_fourth_category,
@@ -48,17 +53,41 @@ public class DF_NewTrans extends DialogFragment {
 
     View view;
 
+    private int layoutId;
+
     Transaction transaction;
 
     RecyclerViewAdapter recyclerViewAdapter;
     RecyclerView recyclerView;
 
 
+    private EditText eTValue;
+    private Button buttonSaveData;
+
+    private MainActivity mainActivity;
+
+    @Override
+    public MainActivity getContext() {
+        return mainActivity;
+    }
+
+    public void setMainActivity(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
 
     private ArrayList<Person> personList;
     private ArrayList<MainCategory> mainCategoryList;
 
     private ArrayList<String> StrList;
+
+
+    public int getLayoutId() {
+        return layoutId;
+    }
+
+    public void setLayoutId(int layoutId) {
+        this.layoutId = layoutId;
+    }
 
     /**
      *
@@ -66,85 +95,34 @@ public class DF_NewTrans extends DialogFragment {
      * @return
      */
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
-        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
-        final LayoutInflater inflater = getActivity().getLayoutInflater();
-
-        view = inflater.inflate(R.layout.layout_carouselview, null);
-        dialogBuilder.setView(view);
-
-
+        ViewGroup rootView = (ViewGroup) inflater.inflate(getLayout(), container, false);
 
         transaction = new Transaction();
+
+        //Personen und Kategorien aus der Datenbank holen
         personList = GlobalVar.Database.getPersons();
         mainCategoryList = GlobalVar.Database.getCategories();
 
 
-        setCarouselView();
+        if(layoutId == 4) {
 
+            buttonSaveData = (Button) rootView.findViewById(R.id.buttonSaveTrans);
+            buttonSaveData.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
-        //Abbrechen-Button
-        dialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+                    mainActivity.readData();
 
-                DF_NewTrans.this.getDialog().cancel();
-            }
-        });
+                }
+            });
 
+        }
 
-
-
-       return dialogBuilder.create();
-
-    }
-
-
-    /**
-     *
-     * @return
-     */
-    private void readData(){
-
-        //Betrag
-        EditText eTvalue = (EditText)view.findViewById(R.id.eTTransValue);
-        String value = eTvalue.getText().toString();
-        transaction.setValue(new Double(value));
-
-        //Personen die gezahlt haben
-        ListView personsPayed = (ListView)view.findViewById(R.id.listViewTransPersons);
-        long[] checkedIds = personsPayed.getCheckedItemIds();
-
-
-        //Personen fuer die der Betrag anfaellt
-
-
-
-        //Kategorie
-
-
-
-        //Details der Ausgabe
-
-
+        return rootView;
 
     }
-
-
-
-    /**
-     *
-     */
-    private void setCarouselView(){
-
-        carouselView = (CarouselView) view.findViewById(R.id.carouselView);
-        carouselView.setPageCount(carouselViews.length);
-        carouselView.setViewListener(viewListener);
-        carouselView.reSetSlideInterval(0);
-
-    }
-
 
 
 
@@ -154,11 +132,11 @@ public class DF_NewTrans extends DialogFragment {
         @Override
         public View setViewForPosition(int position) {
 
-            View customView = getActivity().getLayoutInflater().inflate(carouselViews[position], null);
 
             if(position == 1){
 
-                recyclerView = (RecyclerView)customView.findViewById(R.id.recyclerViewSecondLayout);
+                View view = getView().findViewById(views[1]);
+                recyclerView = (RecyclerView)view.findViewById(R.id.recyclerViewSecondLayout);
                 recyclerView.setHasFixedSize(true);
 
                 LinearLayoutManager llm = new LinearLayoutManager(getActivity());
@@ -169,7 +147,7 @@ public class DF_NewTrans extends DialogFragment {
             }
 
 
-            return customView;
+            return view;
         }
 
 
@@ -198,6 +176,38 @@ public class DF_NewTrans extends DialogFragment {
         recyclerView.setAdapter(recyclerViewAdapter);
 
 
+
+    }
+
+
+    private int getLayout(){
+
+        Log.d("LAYOUTID", String.valueOf(layoutId));
+
+        int layout = 0;
+
+        if(layoutId == 0){
+
+            layout = views[0];
+
+        }else if(layoutId == 1){
+
+            layout = views[1];
+
+        }else if(layoutId == 2){
+
+            layout = views[2];
+
+        }else if(layoutId == 3){
+
+            layout = views[3];
+
+        }else if(layoutId == 4){
+
+            layout = views[4];
+        }
+
+        return layout;
 
     }
 
